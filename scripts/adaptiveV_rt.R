e13cc77a58c79e067f5116a3026876fc169b6709
disandroa <adisandro818@gmail.com>
2022-08-31 15:53:53 -0400

    created RT script and wrote outline

---
+++ scripts/adaptiveV_rt.R
@@ -0,0 +1,52 @@
+# creating CAT vs Full scatters for RT
+
+# Akira Di Sandro, 8.31.22
+
+
+`%notin%` <- Negate(`%in%`)
+# load packages ----
+
+library(psych)
+library(qgraph)
+library(dplyr)
+library(tidyr)
+library(PerFit)
+library(ggplot2)
+library(ggdist)
+library(sdamr)
+library(kableExtra)
+library(matrixStats)
+library(irr)
+library(lubridate)
+library(readr)
+
+
+# load data ----
+
+
+
+# the process
+
+# first, get SD and mean RT from crowdsourced data for each item on each test
+
+
+# itemwise RT from adaptive CNB with be scaled using SDs and mean from above (question number is number from itembank)
+
+
+# QA
+# SMVE
+
+
+# RT QA method
+
+
+# multivariate outlier removal
+
+
+
+
+
+
+
+
+
