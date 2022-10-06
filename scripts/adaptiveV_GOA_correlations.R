7c1fb0063b0fbc3040438d0a57ad26e063583c5a
disandroa <adisandro818@gmail.com>
2022-10-05 23:26:21 -0400

    new scripts for GOA comps and XL CNB RTs

---
+++ scripts/adaptiveV_GOA_correlations.R
@@ -0,0 +1,13 @@
+# Full vs CAT GOA comparisons for AdaptiveV
+
+# Akira Di Sandro, 10.05.2022
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/extralong_test_retest_CNB_RTs.R
@@ -0,0 +1,25 @@
+# Extralong test-retest (CNB RTs) as part of AdaptiveV
+
+# Akira Di Sandro, 10.05.22
+
+
+`%notin%` <- Negate(`%in%`)
+
+# load packages ----
+library(psych)
+library(qgraph)
+library(dplyr)
+library(PerFit)
+library(ggplot2)
+library(ggdist)
+library(sdamr)
+library(kableExtra)
+library(matrixStats)
+library(irr)
+library(lubridate)
+
+
+
+
+
+
